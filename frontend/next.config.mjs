213454e5739370e2644012a50f3487788bdbc447
KrishParekhRMIT <s4043281@student.rmit.edu.au>
2025-04-01 06:05:29 +0530

    hotfix: next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["raw.githubusercontent.com"],
+    remotePatterns: ["raw.githubusercontent.com", "a1-projectgroup-31-krish.s3.us-east-1.amazonaws.com"],
   },
 };
 
